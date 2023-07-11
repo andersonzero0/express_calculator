0c77f752681d154d728dd44deb4b43b20ffa732e
Anderson Viana <92057959+andersonzero0@users.noreply.github.com>
2023-07-11 15:15:38 -0300

    update

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/calculadora.html')
+
+    res.sendFile(__dirname + '/calculadora.html');
+
 })
 
 app.post('/', (req, res) => {
+
     var result = Number(req.body.n1) + Number(req.body.n2)
 
     res.send("Result is " + result)
+    
 })
 
 app.listen(PORT, () => {
